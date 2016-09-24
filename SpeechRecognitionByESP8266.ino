67dd6eefc6951243bb584a8eb0fa632c68a1ec81
Akihiro MATOBA <matobaa@gmail.com>
2016-09-25 01:24:50 +0900

    recognition by bluemix, 10 seconds

--- SpeechRecognitionByESP8266.ino
+++ SpeechRecognitionByESP8266.ino
@@ -1,19 +1,17 @@
+
 /* Flash mode: DIO
- * Flash Freq: 40MHz
- * CPU Freq: 80MHz
- * Flash size: 512K (64K SPIFFS)
- * Debug Port: Disabled
- * Debug Level: Core
- * Reset Method: ck
- * Upload Speed: 115200
- * Port: COM4
- * 
- */
- 
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
+   Flash Freq: 40MHz
+   CPU Freq: 80MHz
+   Flash size: 512K (64K SPIFFS)
+   Debug Port: Disabled
+   Debug Level: Core
+   Reset Method: ck
+   Upload Speed: 115200
+*/
+
+#include <ESP8266HTTPClient.h>
 #include <ESP8266WebServer.h>
-#include <osapi.h>
+// #include <osapi.h>
 #include <FS.h>
 #include <Ticker.h>
 
@@ -23,28 +21,28 @@
 #define DEBUG_MSG(...)
 #endif
 
-#define _BYTE1(x) (  x        & 0xFF )
-#define _BYTE2(x) ( (x >>  8) & 0xFF )
-
 #define FREQUENCY 8000
 #define DURATION 5000
 #define INDICATORPIN 13
 
-const char* host = "api.github.com";
+const char* host = "stream.watsonplatform.net";
 const int httpsPort = 443;
-// Use web browser to view and copy
-// SHA1 fingerprint of the certificate
-const char* fingerprint = "CF 05 98 89 CA FF 8E D8 5E 5C E0 C2 E4 F7 E6 C3 C7 50 DD 5C";
 
+// wifi credentials
 const char* ssid = "07056547823";
 const char* pass = "0466369371";
+// bluemix credentials
+const String username = "0857b1a4-5d6c-4e57-b49d-e4a01164c9ac";
+const String password = "5UFTIalV1g4h";
+const char* fingerprint = "07 AA 36 ED 31 A0 FE BC 5B 41 9E D2 BF 8F 5D 62 05 D2 56 EE";
 
-WiFiClientSecure client;
+String payload = "";
+
 ESP8266WebServer server(80);
 Ticker bufferWriter;
 File fd;
 
-#define BUFBIT 12
+#define BUFBIT 9
 #define BUFSIZE (1<<BUFBIT)
 #define BUFMASK (BUFSIZE-1)
 
@@ -82,22 +80,20 @@ ICACHE_RAM_ATTR void t1IntHandler() {
 
 void flush_buffer() {
   if (digitalRead(INDICATORPIN) == HIGH) return; // do nothing
-  DEBUG_MSG("Flushing Buffer:%d %d\n", 1 - (counter >> BUFBIT) & 1, 1 << (BUFBIT+1));
-  int result = fd.write((uint8_t*)buffer[1 - (counter >> BUFBIT) & 1], 1 << (BUFBIT+1));
-  if(!result) {
-//    Serial.println(SPIFFS_errno(&fd));
+  DEBUG_MSG("Flushing Buffer:%d %d\n", 1 - (counter >> BUFBIT) & 1, 1 << (BUFBIT + 1));
+  int result = fd.write((uint8_t*)buffer[1 - (counter >> BUFBIT) & 1], 1 << (BUFBIT + 1));
+  if (!result) {
+    //    Serial.println(SPIFFS_errno(&fd));
   }
   digitalWrite(INDICATORPIN, HIGH);
 }
 
 // frequency (in hertz) and duration (in milliseconds).
 void start_sampling(unsigned int frequency, unsigned long duration) {
+
   toggle_counts = frequency * (duration / 1000);
   // preparing FS
-  if (!SPIFFS.begin()) {
-    Serial.println("SPIFFS.begin fail");
-    return;
-  }
+
   SPIFFS.format();
   fd = SPIFFS.open("/sample.wav", "w");
   if (!fd) {
@@ -109,7 +105,7 @@ void start_sampling(unsigned int frequency, unsigned long duration) {
   digitalWrite(INDICATORPIN, HIGH);
   DEBUG_MSG("Sampling Started\n");
   // initialize buffer writer
-  bufferWriter.attach(0.1, flush_buffer);
+  bufferWriter.attach(0.02, flush_buffer);
   // initialize timer
   counter = 0;
   timer1_disable();
@@ -123,22 +119,12 @@ void stop_sampling() {
   timer1_disable();
   timer1_detachInterrupt();
   bufferWriter.detach();
+  flush_buffer();
   DEBUG_MSG("Flushing Buffer:%d %d\n", (counter >> BUFBIT) & 1, (counter & BUFMASK) * 2 - 2);
   fd.write((uint8_t*)buffer[(counter >> BUFBIT) & 1], (counter & BUFMASK) * 2 - 2);
+  fd.close();
   digitalWrite(INDICATORPIN, LOW);
   DEBUG_MSG("Sampling Finished\n");
-  fd.close();
-}
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println(ESP.getResetReason());
-
-  wifi_client();
-  // https_connect();
-  init_adc_bias();
-  ets_wdt_disable();
 }
 
 void wifi_client() {
@@ -147,55 +133,57 @@ void wifi_client() {
   WiFi.mode(WIFI_AP_STA);
   WiFi.begin(ssid, pass);
   // WiFi.begin();
-  while (WiFi.status() != WL_CONNECTED) {
+  for (int i = 0; i < 20; i++) {
+    if (WiFi.status() == WL_CONNECTED)  break;
     delay(500);
     Serial.print(".");
   }
-  Serial.println(" connected");
-  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
+/*
+   curl.exe -X POST -u username:password
+   --header "Content-Type: audio/wav"
+   --data-binary @sample.wav
+   https://stream.watsonplatform.net/speech-to-text/api/v1/models/ja-JP_NarrowbandModel/recognize
+*/
 void https_connect() {
-  Serial.print("connecting to ");
-  Serial.println(host);
-  if (!client.connect(host, httpsPort)) {
-    Serial.println("connection failed");
-    return;
-  }
+  HTTPClient client;
+  String url = "https://";
+  url += username;
+  url += ":";
+  url += password;
+  url += "@";
+  url += "stream.watsonplatform.net/speech-to-text/api/v1/models/ja-JP_NarrowbandModel/recognize";
+  client.begin(url, String(fingerprint));
+  client.addHeader("Content-Type", "audio/wav");
+  client.addHeader("Connection", "close");
 
-  Serial.println(client.verify(fingerprint, host) ? "certificate matches" : "certificate doesn't match");
+  Serial.println("Sending for recognize request...");
+  // start connection and send HTTP header
 
-  String url = "/repos/esp8266/Arduino/commits/master/status";
-  Serial.print("requesting URL: ");
-  Serial.println(url);
+  File file = SPIFFS.open("/sample.wav", "r");
+  long size = file.size();
+  DEBUG_MSG("Content-Length: %d\n", size);
+  int httpCode = client.sendRequest("POST", &file, size);
+  Serial.printf("Request for recognize sent.\n", size);
+  file.close();
 
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "User-Agent: BuildFailureDetectorESP8266\r\n" +
-               "Connection: close\r\n\r\n");
+  // httpCode will be negative on error
+  if (httpCode > 0) {
+    // HTTP header has been send and Server response header has been handled
+    DEBUG_MSG("[HTTP] GET... code: %d\n", httpCode);
 
-  Serial.println("request sent");
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    Serial.println(line);    if (line == "\r") {
-      Serial.println("headers received");
-      break;
+    // file found at server
+    if (httpCode == HTTP_CODE_OK) {
+      payload = client.getString();
+      Serial.println(payload.c_str());
     }
-  }
-  String line = client.readStringUntil('\n');
-  if (line.startsWith("{\"state\":\"success\"")) {
-    Serial.println("esp8266/Arduino CI successfull!");
   } else {
-    Serial.println("esp8266/Arduino CI has failed");
+    DEBUG_MSG("[HTTP] GET... failed, error: %s\n", client.errorToString(httpCode).c_str());
   }
-  Serial.println("reply was:");
-  Serial.println("==========");
-  //Serial.println(line);
-  Serial.println("==========");
-  Serial.println("closing connection");
+  client.end();
 }
-
 
 void doGet() {
   String html = "<h1>WiFi Settings</h1>";
@@ -204,42 +192,43 @@ void doGet() {
   html += "  <input type='text' name='pass' placeholder='pass'><br>";
   html += "  <input type='submit'><br>";
   html += "</form>";
-  html += "<a href='start'>start</a> ";
-  html += "<a href='sample.wav'>sample.wav</a>";
+  html += "<a href='start'>sampling</a> ";
+  html += "<a href='sample.wav'>download</a> ";
+  html += "<a href='recognize'>recognize</a> ";
+  html += payload;
   server.send(200, "text/html", html);
 }
 
-struct {
-  char riff[4];
-  int32_t len1;
-  char wave[4];
-  char fmt[4];
-  int32_t formatSize;
-  int16_t formatCode;
-  int16_t channelCount;
-  int32_t samplingRate;
-  int32_t bytesPerSecond;
-  int16_t bytesPerBlock;
-  int16_t bitsPerSample;
-  char data[4];
-  int32_t len2;
-} riff_header;
-
 void writeRiffHeader(Stream *fd) {
+  struct {
+    char riff[4];
+    int32_t len1;
+    char wave[4];
+    char fmt[4];
+    int32_t formatSize;
+    int16_t formatCode;
+    int16_t channelCount;
+    int32_t samplingRate;
+    int32_t bytesPerSecond;
+    int16_t bytesPerBlock;
+    int16_t bitsPerSample;
+    char data[4];
+    int32_t len2;
+  } riff_header;
   int size = sizeof(riff_header) - 4 + 4 + FREQUENCY * (DURATION / 1000) * 2;
   strncpy(riff_header.riff, "RIFF", 4);
   riff_header.len1 = size;
   strncpy(riff_header.wave, "WAVE", 4);
-  strncpy(riff_header.fmt, "fmt ", 4);              // start of format chunk
-  riff_header.formatSize = 16;        // format size
-  riff_header.formatCode = 1;         // uncompressed PCM
-  riff_header.channelCount = 1;       // 1=monoral
-  riff_header.samplingRate = FREQUENCY;
+  strncpy(riff_header.fmt, "fmt ", 4);  // start of format chunk
+  riff_header.formatSize = 16;
+  riff_header.formatCode = 1;  // uncompressed PCM
+  riff_header.channelCount = 1; // monoral
+  riff_header.samplingRate = FREQUENCY; // Heltz
   riff_header.bytesPerSecond = FREQUENCY * 2;
-  riff_header.bytesPerBlock = 2;      // 16bit monoral -> 2byte
-  riff_header.bitsPerSample = 16;
-  strncpy(riff_header.data, "data", 4); // start of data chunk
-  riff_header.len2 = FREQUENCY * (DURATION / 1000) * 2; // wave data size
+  riff_header.bytesPerBlock = 2;  // 16bit monoral -> 2byte
+  riff_header.bitsPerSample = 16; // 16bit
+  strncpy(riff_header.data, "data", 4);  // start of data chunk
+  riff_header.len2 = FREQUENCY * (DURATION / 1000) * 2;  // wave data size
   fd->write((uint8_t*)&riff_header, sizeof(riff_header));
 }
 
@@ -251,7 +240,8 @@ void doGetWave() {
 
 void startRecording() {
   start_sampling(FREQUENCY, DURATION);
-  doGet();
+  server.sendHeader("Location", "/");
+  server.send(302);
 }
 
 void doPost() {
@@ -264,10 +254,17 @@ void doPost() {
   server.send(200, "text/html", html);
 }
 
+void doRecognize() {
+  server.sendHeader("Location", "/");
+  server.send(302);
+  https_connect();
+}
+
 void webserver() {
   server.on("/", HTTP_GET, doGet);
   server.on("/sample.wav", HTTP_GET, doGetWave);
   server.on("/start", HTTP_GET, startRecording);
+  server.on("/recognize", HTTP_GET, https_connect);
   server.on("/", HTTP_POST, doPost);
   server.begin();
   Serial.println("HTTP server started.");
@@ -277,24 +274,25 @@ void webserver() {
   }
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  start_sampling(FREQUENCY, DURATION);
-  delay(5000);
-  webserver();
-  Serial.println("loop end");
-  delay(0);
-  // delayMicroseconds(125);
-}
+void setup() {
+  Serial.begin(115200);
+  Serial.print("Reset by ");
+  Serial.println(ESP.getResetReason());
 
-void escaped() {
-  // read the input on analog pin 0:
-  // if the server's disconnected, stop the client:
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println(WiFi.status());
-    WiFi.printDiag(Serial);
-    Serial.println("disconnecting from server.");
-    webserver();
+
+  init_adc_bias();
+  if (!SPIFFS.begin()) {
+    Serial.println("SPIFFS.begin fail");
+    return;
   }
+  ets_wdt_disable();
+
+  //   start_sampling(FREQUENCY, DURATION);
+  delay(5000);
+  wifi_client();
+  //  https_connect();
+  webserver();
 }
 
+void loop() {
+}
